2e1490b52869145a25a1aa5560185f4da804df1d
Steven Hejnas <shejnas91@gmail.com>
2020-03-09 12:45:51 -0400

    trading places

--- bash.js
+++ bash.js
@@ -1,8 +1,15 @@
+let imprt = require('./pwd')
+
+
+
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
-    
+
   const cmd =  data.toString().trim();
-  if(cmd === 'pwd')  process.stdout.write(`${process.cwd()}`)
-//   process.stdout.write('You typed: ' + cmd);
+  if(cmd === "pwd"){
+    process.stdout.write(`${imprt}`);
+  }
+  process.stdout.write('You typed: ' + cmd);
+
   process.stdout.write('\nprompt > ');
 })
