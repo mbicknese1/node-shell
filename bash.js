3f81afb4dd77d16fd6876f0d6290437ac286a228
Iskak Mantyubetov <ayzik55@gmail.com>
2020-03-09 12:25:17 -0400

    pwd

--- bash.js
+++ bash.js
@@ -1,6 +1,8 @@
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
+    
   const cmd =  data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
+  if(cmd === 'pwd')  process.stdout.write(`${process.cwd()}`)
+//   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 })
