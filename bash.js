397304461aa1d44899a0d1c73a00a70da53627ae
Matt Bicknese <mbicknes@villanova.edu>
2020-03-09 13:17:35 -0400

    new commit

--- bash.js
+++ bash.js
@@ -1,5 +1,5 @@
 let imprt = require('./pwd')
-
+let ls = require('./ls')
 
 
 process.stdout.write('prompt > ');
@@ -7,9 +7,15 @@ process.stdin.on('data', (data) => {
 
   const cmd =  data.toString().trim();
   if(cmd === "pwd"){
-    process.stdout.write(`${imprt}`);
+    process.stdout.write(`${imprt()}`);
   }
-  process.stdout.write('You typed: ' + cmd);
+  else if (cmd === "ls") {
+      process.stdout.write(`${ls()}`);
+  }
+  else {
+      process.stdout.write('You typed: ' + cmd);
+      process.stdout.write('\nprompt > ');
+    }
 
-  process.stdout.write('\nprompt > ');
+//   process.stdout.write('\nprompt > ');
 })
--- pwd.js
+++ pwd.js
@@ -1,11 +1,6 @@
 
 module.exports = function(){
   //process.stdout.write('prompt > ');
-  process.stdin.on('data', (data) => {
-
-    const cmd =  data.toString().trim();
-    process.stdout.write(`${process.cwd()}`)
-  
+    process.stdout.write(`${process.cwd()}`);
     process.stdout.write('\nprompt > ');
-  })
 }
